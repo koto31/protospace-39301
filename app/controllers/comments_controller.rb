a70e216c5bccd41749b0c42a9eaed5c5f0b3e672
koto31 <136156845+koto31@users.noreply.github.com>
2023-09-11 07:38:52 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+ def create
+   @comment = Comment.create(comment_params)
+   if @comment.save
+     redirect_to root_path
+   else
+     @comment = Comment.new
+     @comments = @prototype.comments.includes(:user)
+     render root_path
+   end
+  end
+ private
+  def comment_params
+    params.require(:comment).permit(:text, :content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
